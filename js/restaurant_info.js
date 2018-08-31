2a1e48a55bc799900436af8143c9a947f51e3ed2
Cynth42 <cclinc2005@yahoo.com>
2018-08-31 00:54:40 -0400

    Add margin: 0 to body element

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken:  '<your MAPBOX API KEY HERE>',
+        mapboxToken:  'pk.eyJ1IjoiY3ludGgyMDE4IiwiYSI6ImNqa3Zxb3d2eTB0ejEzcXBhYjAycWh4d3AifQ.SnN_pIidmpVNkkaBbF3OPA', //'<your MAPBOX API KEY HERE>',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -91,7 +91,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   image.alt = restaurant.image;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
-//  image.alt = DBHelper.imageAltForRestaurant(restaurant);
+ //image.alt = DBHelper.imageAltForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
